55432591d79d5e9820b5541911edb39c3e9aa1f8
Benard Khajira <benardkhajira@gmail.com>
2024-05-25 16:32:09 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,21 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+// Define the <About> component here
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home /> {/* Add the <Home> component here */}
+      <About /> {/* Add the <About> component here */}
     </div>
   );
 }
